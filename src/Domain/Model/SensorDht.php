7a50a21329d2a0f2ef917c0888cc9397e4571a3f
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-06-21 22:22:07 -0300

    Classe para instanciar objeto SensorDht

---
+++ src/Domain/Model/SensorDht.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Weather\Iot\Domain\Model;
+
+class SensorDht
+{
+    public function __construct(
+        public readonly ?int $id,
+        public readonly string $name,
+        public readonly \DateTimeInterface $time,
+        public readonly float $temperature,
+        public readonly float $humidity,
+    )
+    {
+        
+    }
+
+}
