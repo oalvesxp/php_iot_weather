0d00126c3e8eb6ed0fa353f7ce39f46a0fcf8a39
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-06-24 16:47:06 -0300

    Preenchendo a tabela de dados com php

--- src/Controller/DataTablesController.php
+++ src/Controller/DataTablesController.php
@@ -2,10 +2,16 @@
 
 namespace Weather\Iot\Controller;
 
+use Weather\Iot\Infraestructure\Repository\WeatherSensorDhtRepository;
+
 class DataTablesController implements Controller
 {
+    public function __construct(private WeatherSensorDhtRepository $repository) 
+    {}
+
     public function processRequest(): void
-    {        
+    {   
+        $sensors = $this->repository->getAll();
         require_once __DIR__ . '/../../views/tables.php';
     }
 }
