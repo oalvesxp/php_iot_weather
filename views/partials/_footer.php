d573360c32eb75d98f95b183149c0b1df524733e
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-06-24 11:49:39 -0300

    Criando as Views partials

---
+++ views/partials/_footer.php
@@ -0,0 +1,6 @@
+
+    <!-- Scripts Data Tables -->
+    <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
+    
+</body>
+</html>
---
+++ views/partials/_header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SRV Químico</title>
+
+    <!-- CSS Data Tables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css" type="text/css" />
+</head>
+<body>
