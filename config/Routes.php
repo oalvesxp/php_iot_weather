703b110b259dafc0c94e123eeddceb3f87abc2c7
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-06-25 11:24:40 -0300

    Criando novo controler para diferenciar tabelas de lastHour e FullHistory

--- config/Routes.php
+++ config/Routes.php
@@ -2,6 +2,7 @@
 
 return [
     'GET|/' => \Weather\Iot\Controller\HomeController::class,
-    'GET|/tables' => \Weather\Iot\Controller\DataTablesController::class,
+    'GET|/table-short' => \Weather\Iot\Controller\ShortTableController::class,
+    'GET|/table-full' => \Weather\Iot\Controller\FullTableController::class,
     'GET|/faq' => \Weather\Iot\Controller\FaqController::class,
 ];
--- src/Controller/ShortTableController.php
+++ src/Controller/ShortTableController.php
@@ -4,14 +4,14 @@ namespace Weather\Iot\Controller;
 
 use Weather\Iot\Infraestructure\Repository\WeatherSensorDhtRepository;
 
-class DataTablesController implements Controller
+class ShortTableController implements Controller
 {
     public function __construct(private WeatherSensorDhtRepository $repository) 
     {}
 
     public function processRequest(): void
     {   
-        $sensors = $this->repository->getAll();
-        require_once __DIR__ . '/../../views/tables.php';
+        $sensors = $this->repository->getLastHour();
+        require_once __DIR__ . '/../../views/table-short.php';
     }
 }
