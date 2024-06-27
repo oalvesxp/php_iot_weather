e87348cef166b12291dce2ef90f72386260ddaff
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-06-27 16:59:20 -0300

    configurando conexao wifi

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,9 +1,14 @@
 #include "DHT.h"
+#include "WiFi.h"
 
 /** Declarando portas e modelo do sensor */
 #define DHT1PIN 4
 #define DHT2PIN 5
 #define DHTTYPE DHT11
+
+/** Credenciais do WiFi */
+const char* ssid = "nome-da-rede";
+const char* password = "senha";
 
 /** Declarando sensores */
 DHT dht1(DHT1PIN, DHTTYPE);
@@ -11,9 +16,24 @@ DHT dht2(DHT2PIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
+
+  /** Configurações do WiFi */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("\nConectando");
+
+  while(WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      delay(1000);
+  }
+
+  Serial.println("\nConectato a rede WiFi");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
+
+  /** Inicializando os sensores */
   dht1.begin();
   dht2.begin();
-
 }
 
 void loop() {
@@ -28,15 +48,13 @@ void loop() {
   // Check if any reads failed and exit early (to try again).
   if (isnan(s01h) || isnan(s01t)) {
     Serial.println(F("Não foi possível ler o sensor: Pintura 01"));
-    return;
   }
 
   if (isnan(s02h) || isnan(s02t)) {
     Serial.println(F("Não foi possível ler o sensor: Verniz 01"));
-    return;
   }
 
-  Serial.print(F("Pintura 01 - "));
+  Serial.print(F("\nPintura 01 - "));
   Serial.print(F("Humidity: "));
   Serial.print(s01h);
   Serial.print(F("%  Temperature: "));
@@ -50,7 +68,6 @@ void loop() {
   Serial.print(s02t);
   Serial.print(F("°C \n"));
 
-  Serial.print(F("\n"));
-  delay(10000);
+  delay(1000);
 
 }
