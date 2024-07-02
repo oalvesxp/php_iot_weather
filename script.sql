cc45c13645d97eb31dad95d8a6c0d981edc7f11b
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-07-02 15:58:32 -0300

    Script para criar tabela do banco de dados

---
+++ script.sql
@@ -0,0 +1,9 @@
+CREATE TABLE WT0010
+    ( 
+        WT0_ID INT NOT NULL AUTO_INCREMENT
+        , WT0_NAME VARCHAR(50)
+        , WT0_TIME VARCHAR(50)
+        , WT0_TEMP FLOAT(0.0)
+        , WT0_HUM FLOAT(0.0)
+        , PRIMARY KEY (WT0_ID)
+    );
